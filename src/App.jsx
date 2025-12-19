ce1cc8291d6b71bdab30efde9fb358dde6778444
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-19 22:42:31 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="text-center p-8 bg-white rounded-lg shadow-lg"></div>
+        <div className="text-center p-8 bg-white rounded-lg shadow-lg">
+            <h1 className="text-4xl font-bold text-primary mb-4">Haki Yetu Digital</h1>
+
 
 
 
