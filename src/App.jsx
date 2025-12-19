5cfb15cb4a46822f43daa224b632dc84b479fc2b
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-19 22:41:24 +0300

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="text-center p-8 bg-white rounded-lg shadow-lg"></div>
+
+
+
