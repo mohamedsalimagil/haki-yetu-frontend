3d31edaa6c24513b51f81470867affc5fa99f2ca
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-20 21:33:58 +0300

    react-router-dom for navigation between different routes

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,1 +1,2 @@
 import React, { useState } from 'react'; //react library for building user interfaces
+import { Link } from 'react-router-dom';//react-router-dom for navigation between different routes
