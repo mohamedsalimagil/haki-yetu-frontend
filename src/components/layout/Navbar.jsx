69c2edfbc34ea6d600dd39fae6c10ce9ffa9a0b4
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-20 21:35:22 +0300

    importing icons from lucid react library

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,2 +1,3 @@
 import React, { useState } from 'react'; //react library for building user interfaces
 import { Link } from 'react-router-dom';//react-router-dom for navigation between different routes
+import { Menu, X, Scale } from 'lucide-react';//
