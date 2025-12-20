bd3e5cbec793b06b5be8a03323598a408280c45c
Beatrice Wambui <bettyneema2@gmail.com>
2025-12-20 19:52:19 +0300

    feat(register page): create register page wrapper component for user onboarding routing

---
+++ src/pages/auth/Register.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterForm from '../../components/domain/auth/RegisterForm';
+
+const Register = () => {
+  return <RegisterForm />;
+};
+
+export default Register;
