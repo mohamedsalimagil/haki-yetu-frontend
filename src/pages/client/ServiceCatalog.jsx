659af7c514fc4aa1230a069683829e3f5fcc7b75
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-21 22:16:51 +0300

    state to hold search term

--- src/pages/client/ServiceCatalog.jsx
+++ src/pages/client/ServiceCatalog.jsx
@@ -4,5 +4,6 @@ import ServiceCard from '../../components/domain/marketplace/ServiceCard';
 import { Search } from 'lucide-react';
 
 const ServiceCatalog = () => { 
-    const [services, setServices] = useState([]);
+    const [services, setServices] = useState([]);//state to hold service data
     const [loading, setLoading] = useState(true);//state to manage loading 
+    const [searchTerm, setSearchTerm] = useState('');// state to hold search term 
