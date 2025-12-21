461e51b7463f476ad1aff60bd3770645381d5f54
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-21 22:14:28 +0300

    service catalog page

--- src/pages/client/ServiceCatalog.jsx
+++ src/pages/client/ServiceCatalog.jsx
@@ -1,3 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import ServiceCard from '../../components/domain/marketplace/ServiceCard';
+import { Search } from 'lucide-react';
+
+const ServiceCatalog = () => { 
