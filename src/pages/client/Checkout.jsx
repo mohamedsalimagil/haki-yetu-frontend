d5f6f3c7351f84e88e49703d55057c744fa77de3
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-12-24 00:12:03 +0300

    "feat: define Checkout component function"

--- src/pages/client/Checkout.jsx
+++ src/pages/client/Checkout.jsx
@@ -2,3 +2,5 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Smartphone, Lock, CheckCircle, Loader } from 'lucide-react';
 import api from '../../services/api';
+
+const Checkout = () => { // Define functional component for checkout page
